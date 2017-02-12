133c825758acdcd70d6db6377b4ddb272acde9ae
Dom Ronan <dominickronan@doms-mbp.bc.edu>
2017-02-12 15:00:49 -0500

    added segue, bar buttons, and navigation controllers

--- To Do List/DetailViewController.swift
+++ To Do List/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  To Do List
 //
 //  Created by Dom Ronan on 2/11/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
